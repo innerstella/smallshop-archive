baff8eb71827cb1600e6504f3604c96bbea177fb
innerstella <suji0509@naver.com>
2026-01-18 22:38:08 +0900

    remove: 배너

--- src/data/bannerData.ts
+++ src/data/bannerData.ts
@@ -1,8 +1,4 @@
-// import BannerStella from "../assets/banner/stella.jpeg"
 import BannerApp from "../assets/banner/app.jpg";
-import BannerLoud1 from "../assets/banner/banner1.png";
-import BannerLoud2 from "../assets/banner/banner2.png";
-import BannerLoud3 from "../assets/banner/banner3.png";
 import BannerHyunjae from "../assets/banner/hyunjae.jpeg";
 import BannerJJ from "../assets/banner/jangjun.webp";
 import BannerReview from "../assets/banner/review.jpg";
@@ -14,28 +10,13 @@ export const BannerData = [
     src: BannerReview,
   },
   {
-    name: "라우드1",
-    src: BannerLoud1,
-    link: "https://www.loud.kr/news/event/79683?utm_source=tv&utm_medium=banner1",
-  },
-  {
     name: "store",
     src: BannerStore,
-  },
-  {
-    name: "라우드2",
-    src: BannerLoud2,
-    link: "https://www.loud.kr/news/event/79683?utm_source=tv&utm_medium=banner2",
   },
   {
     name: "hyunjae",
     src: BannerHyunjae,
     link: "https://youtube.com/@leejaehyun_ing?si=eELbbF09cnmp7ve0",
-  },
-  {
-    name: "라우드3",
-    src: BannerLoud3,
-    link: "https://www.loud.kr/news/event/79683?utm_source=tv&utm_medium=banner3",
   },
   {
     name: "jangjun",
